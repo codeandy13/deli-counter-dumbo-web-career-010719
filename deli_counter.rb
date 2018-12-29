f5be311dbd7afc541a0d718b344bc5a47f033d90
Andrew Kim <akim618@gmail.com>
2018-12-29 01:05:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require "pry"
+
+
+def line(katz_deli)
+
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+
+    message = "The line is currently"
+    katz_deli.each do |name|
+      message += name
+    end
+    puts message
+  end
+
+end
